39b9a50bd4423ece4b521f7de15a38d253b19bca
amenlemma1212-cloud <amenlemma.1212@gmail.com>
2026-06-06 14:17:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-# የገጹ መለያ ስም
-st.set_page_config(page_title="Abel Browser", page_icon="🌐", layout="wide")
+st.set_page_config(page_title="Abel Browser", page_icon="🌐", layout="centered")
 
 st.title("Abel Browser 🌐")
-st.write("እንኳን ወደ አቤል ብሮውዘር በደህና መጡ!")
+st.write("እንኳን ወደ አቤል ብሮውዘር በደህና መጡ! ከታች መፈለግ የሚፈልጉትን ሊንክ ያስገቡ።")
 
 # የሊንክ መጻፊያ ሳጥን
 url = st.text_input("የዌብሳይት ሊንክ እዚህ ያስገቡ (ለምሳሌ፦ google.com)፦", "https://www.google.com")
@@ -12,8 +11,6 @@ url = st.text_input("የዌብሳይት ሊንክ እዚህ ያስገቡ (ለም
 if not url.startswith("http"):
     url = "https://" + url
 
-# ዌብሳይቱን በStreamlit ውስጥ መክፈቻ
-st.markdown(
-    f'<iframe src="{url}" style="width:100%; height:700px; border:none;"></iframe>',
-    unsafe_allow_html=True
-)
+# ተጠቃሚው ቁልፉን ሲነካ በቀጥታ ዌብሳይቱን በአዲስ ታብ ይከፍታል
+st.write("---")
+st.link_button("🚀 ድረ-ገጹን ለመክፈት እዚህ ይንኩ (Go)", url, use_container_width=True)
